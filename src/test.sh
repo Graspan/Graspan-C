6586bcf1e0813996a9aef63377d18c97d6e897f4
pcj600 <1900876384@qq.com>
2019-02-25 22:57:34 -0800

    修改测试脚本中的错误，添加log文件夹

--- src/test.sh
+++ src/test.sh
@@ -23,7 +23,7 @@ runDiskSpa(){
 	for file in $(ls $1) 
 	do
 		logFile="log/$file.$NUMPARTITIONS.$MEMBUDGET.$NUMTHREADS.output"
-		../bin/comp $1/$file $RULES_DATAFLOW $2 $3 $4 $DATASTRUCTURE > $logFile
+		../bin/comp $1/$file $2 $3 $4 $5 $DATASTRUCTURE > $logFile
 		preTime=$(tail -3 $logFile | head -1)
 		computeTime=$(tail -2 $logFile | head -1)
 		totalTime=$(tail -1 $logFile | head -1)
@@ -33,14 +33,14 @@ runDiskSpa(){
 }
 
 echo "DiskSpa test running..."
-runDiskSpa $DATAFLOW_DIR/Apache_Httpd_2.2.18_Dataflow $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
-runDiskSpa $DATAFLOW_DIR/PostgreSQL_8.3.9_Dataflow $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
+runDiskSpa $DATAFLOW_DIR/Apache_Httpd_2.2.18_Dataflow $RULES_DATAFLOW $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
+runDiskSpa $DATAFLOW_DIR/PostgreSQL_8.3.9_Dataflow $RULES_DATAFLOW $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
 
-runDiskSpa $POINTSTO_DIR/Apache_Httpd_2.2.18_Points-to $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
-runDiskSpa $POINTSTO_DIR/Hdfs_Points-to $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
-runDiskSpa $POINTSTO_DIR/Mapreduce_Points-to $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
-runDiskSpa $POINTSTO_DIR/PostgreSQL_8.3.9_Points-to $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
+runDiskSpa $POINTSTO_DIR/Apache_Httpd_2.2.18_Points-to $RULES_POINTSTO $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
+runDiskSpa $POINTSTO_DIR/Hdfs_Points-to $RULES_POINTSTO $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
+runDiskSpa $POINTSTO_DIR/Mapreduce_Points-to $RULES_POINTSTO $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
+runDiskSpa $POINTSTO_DIR/PostgreSQL_8.3.9_Points-to $RULES_POINTSTO $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
 
-#runDiskSpa $DATAFLOW_DIR/Linux_dataflow_data $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
-#runDiskSpa $POINTSTO_DIR/Linux_pointsto_data $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
+#runDiskSpa $DATAFLOW_DIR/Linux_dataflow_data $RULES_DATAFLOW $NUMPARTITIONS $MEMBUDGET $NUMTHREADS 
+#runDiskSpa $POINTSTO_DIR/Linux_pointsto_data $RULES_POINTSTO $NUMPARTITIONS $MEMBUDGET $NUMTHREADS
 echo "DiskSpa test finished"
